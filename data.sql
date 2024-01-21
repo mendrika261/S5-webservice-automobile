22f3b6611d8604332465e543a9699f1b05742a5f
Restyled.io <commits@restyled.io>
2024-01-21 16:17:48 +0000

    Restyled by pg_format

--- data.sql
+++ data.sql
@@ -1,4 +1,9 @@
-INSERT INTO public.page (id, exact, level, url) VALUES ('0', true, 0, '/connexion');
-INSERT INTO public.page (id, exact, level, url) VALUES ('1', false, 10, '/deconnexion');
+INSERT INTO public.page (id, exact, level, url)
+    VALUES ('0', TRUE, 0, '/connexion');
 
-INSERT INTO public.utilisateur (id, level, email, mot_de_passe) VALUES ('0', 10, 'user@user.com', '1234');
+INSERT INTO public.page (id, exact, level, url)
+    VALUES ('1', FALSE, 10, '/deconnexion');
+
+INSERT INTO public.utilisateur (id, level, email, mot_de_passe)
+    VALUES ('0', 10, 'user@user.com', '1234');
+
